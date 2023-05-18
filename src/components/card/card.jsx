e23feb8240e8443c170e1778c3d88a0cc489be4e
andrelara2002 <andrelara2002@gmail.com>
2023-05-17 22:56:34 -0300

    Style: Ajustments in logotypes and colors

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -18,22 +18,22 @@ export default function Card({ Logo, title, description, link, github, language 
         <section className="info p-5">
             <section className="text ">
                 <section className="flex justify-between">
-                    <strong className="text-slate-700 text-xl capitalize dark:text-gray-100 ">{title}</strong>
-                    {Logo && <Logo.icon size={15} color={Logo.color} />}
+                    <strong className="text-slate-800 text-xl capitalize dark:text-gray-100 ">{title}</strong>
+                    {Logo && <Logo.icon size={18} color={Logo.color} />}
                 </section>
                 <Spacer />
-                <p className="description text-slate-400 line-clamp-3 dark:text-slate-300">{description}</p>
+                <p className="description text-slate-500 line-clamp-3 dark:text-slate-300">{description}</p>
             </section>
 
         </section>
-        <section className={`actions ${!link ? 'col-1' : 'col-2'} border-t-2  dark:border-zinc-950 border-gray-100`}>
+        <section className={`actions ${!link ? 'col-1' : 'col-2'} border-t  dark:border-zinc-950 border-gray-200`}>
             <button onClick={() => handleWebsite(github)} className="action dark:border-zinc-900 border-gray-100 hover:bg-pink-500 hover:rounded-lg">
                 <AiOutlineGithub color={isDark() ? '#fff' : '#000'} />
                 <strong className="text-gray-900 dark:text-gray-100">Github</strong>
             </button>
             {
                 link ?
-                    <button onClick={() => handleWebsite(link)} className="action border-l-2   dark:border-gray-950 hover:bg-pink-500 hover:rounded-lg">
+                    <button onClick={() => handleWebsite(link)} className="action border-l  dark:border-gray-950 hover:bg-pink-500 hover:rounded-lg">
                         <BiLinkExternal color={isDark() ? '#fff' : '#000'} />
                         <strong className="text-gray-900 dark:text-gray-100">Acessar</strong>
                     </button>
--- src/services/Logotypes.js
+++ src/services/Logotypes.js
@@ -1,18 +1,20 @@
-import { IoLogoJavascript, IoLogoHtml5, IoLogoCss3, } from "react-icons/io"
-import { SiFlutter, SiDart } from 'react-icons/si'
+import { IoLogoHtml5, IoLogoCss3, } from "react-icons/io"
+import { SiFlutter, SiDart, SiCplusplus, SiCsharp, SiJavascript } from 'react-icons/si'
 import { FaReact, FaJava } from 'react-icons/fa'
 import { DiCode, DiPython } from 'react-icons/di'
-import { TbBrandCSharp } from 'react-icons/tb'
+
+import isDark from "./ThemeDetector"
 
 export const Logotypes = {
-    'javascript': { icon: IoLogoJavascript, color: '#FCBA28' },
+    'javascript': { icon: SiJavascript, color: '#FCBA28' },
     'css': { icon: IoLogoCss3, color: '#4682B4' },
-    'c#': { icon: TbBrandCSharp, color: '#A3568C' },
+    'c#': { icon: SiCsharp, color: '#A3568C' },
     'flutter': { icon: SiFlutter, color: '#3D9DD9' },
     'dart': { icon: SiDart, color: '#05AFF2' },
     'html': { icon: IoLogoHtml5, color: '#C70039' },
     'java': { icon: FaJava, color: '#FF7A19' },
     'react': { icon: FaReact, color: '' },
-    'code': { icon: DiCode, color: '#0f0f0f' },
-    'python': { icon: DiPython, color: '#EFCA33' }
+    'code': { icon: DiCode, color: isDark() ? '#fafafa' : '#0f0f0f' },
+    'python': { icon: DiPython, color: '#EFCA33' },
+    'c++': { icon: SiCplusplus, color: '#3987A3' },
 }
